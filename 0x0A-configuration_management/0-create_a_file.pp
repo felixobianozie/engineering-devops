b0b23a14772eae2e4fc6fb1c49a7d69021df5869
felixobianozie <felixobianozie@gmail.com>
2022-09-16 21:57:31 +0000

    Included 0-create_a_file.pp

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# This manifest contains the declaration of a file resource.
+file {'/tmp/school':
+  ensure  => 'file',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet',
+}
